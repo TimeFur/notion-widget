59349b39fe882e62f20889212ca0103b8d23079a
Wayne <djs860403@gmail.com>
2023-03-11 00:09:47 +0800

    Setting express to start server
    1. Starting server to listen request.
    2. Setting default json and url encoded.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import dotenv from "dotenv"
+import express from "express"
 import { updatePages, readDatabase } from './NotionInterface.js';
 
 dotenv.config({ DB_ID: process.env.DATABASE_ID });
 
 const DB_ID = process.env.DATABASE_ID
-
 const databaseId = DB_ID
-
 /**************************************
  *          Setting config
  **************************************/
@@ -16,9 +15,34 @@ const db_filter = {
     //     'equals': true
     // }
 }
-readDatabase(databaseId, db_filter)
-    .then((dbList) => {
-        console.log(dbList)
-    })
+// readDatabase(databaseId, db_filter)
+//     .then((dbList) => {
+//         console.log(dbList)
+//     })
 // updatePages('0104c2c4-64f6-438a-aaf6-103731d25817')
 // console.log(notion.databases.update)
+
+/**************************************
+ *          Server start
+ **************************************/
+var app = express()
+
+//support json-encoded and url-encoded bodies
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//define get & post request
+app.get('/', (req, res) => {
+    //.params is grab specific parameter
+    res.send("Hello", req.params, req.query)
+
+})
+app.post('/updatePage', (req, res) => {
+    console.log(req.body)
+})
+
+//server start listen
+var server = app.listen(process.env.PORT, () => {
+    var host = server.address().address
+    var localPort = server.address().port
+})
